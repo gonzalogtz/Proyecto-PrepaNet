5f66c98754c4ca0bd290e6696c80dfdfaad9e837
Armando Galván <armandocgv@gmail.com>
2017-09-24 21:46:11 -0500

    Login css and html (not working)

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :reporte_quincenals
   resources :conglomerado_quincenals
-  root 'mainmenututor#MenuTutor'
+  root 'users#login.html'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
