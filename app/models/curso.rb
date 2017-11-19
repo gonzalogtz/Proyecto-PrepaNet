da9c09f1c68cddfff8729cc79d467e53e9aaeba4
David Valles <dvalles@Davids-MacBook-Pro.local>
2017-11-19 14:25:23 -0600

    Excels

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,2 +1,24 @@
 class Curso < ApplicationRecord
+    CSV_MAP = {
+        'Campus' => 'campus',
+        'Curso' => 'clave_materia',
+        'Grupo' => 'grupo',
+    }
+
+    def self.import(file)
+        CSV.foreach(file.path, headers:true) do |row|
+            curso_hash = row.to_hash
+            curso = Curso.where(grupo: curso_hash["grupo"])
+
+            attributes = CSV_MAP.each_with_object({}) do |(csv_key, attribute_key), result|
+                result[attribute_key] = row[csv_key]
+            end
+            
+            if curso.count == 1
+                curso.first.update_attributes(attributes)
+            else
+                Curso.create!(attributes)
+            end
+        end
+    end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,14 +1,34 @@
 class Usuario < ApplicationRecord
     require 'csv'
+
+    CSV_MAP = {
+       'Cuenta' => 'cuenta',
+       'Nómina/Matrícula' => 'nomina_matricula' ,
+       'Contraseña' => 'contrasena',
+       'Campus' => 'campus', 
+       'Rol' => 'rol',
+       'Título' => 'titulo',
+       'Nombres' => 'nombres',
+       'A.Paterno' => 'apellido_p',
+       'A.Materno' => 'apellido_m',
+       'E-mail' => 'correo',
+       'Tel. Directo' => 'telefono',
+       'Periodo' => 'periodo',
+    }
+
     def self.import(file)
         CSV.foreach(file.path, headers:true) do |row|
             user_hash = row.to_hash
-            user = Usuario.where(id: user_hash["cuenta"])
+            user = Usuario.where(cuenta: user_hash["cuenta"])
+
+            attributes = CSV_MAP.each_with_object({}) do |(csv_key, attribute_key), result|
+                result[attribute_key] = row[csv_key]
+            end
             
             if user.count == 1
-                user.first.update_attributes(user_hash)
+                user.first.update_attributes(attributes)
             else
-                Usuario.create!(user_hash)
+                Usuario.create!(attributes)
             end
         end
     end
